1b8e1757786ad9b9de42673e3b074bd08e8bb391
akamatsu <akamatsu@star-prince.com>
2019-09-28 22:14:10 +0900

    function components

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import React,{Component} from 'react';
 //     <div><h1>Hello World</h1></div>
 //   );
 // }
-class App extends Component{
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("I am clicked")}}/>
-      </React.Fragment>
+// class App extends Component{
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("I am clicked")}}/>
+//       </React.Fragment>
+//     )
+//   }
+// }
+const App = () =>{
+  return (<div>
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
     )
-  }
+}
+const Cat = () =>{
+  return <div>Meow!</div>
 }
 export default App;
 　
