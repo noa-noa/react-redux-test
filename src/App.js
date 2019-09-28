a88c3315ba72a1bd09a5abe46f3fa169d22846d9
akamatsu <akamatsu@star-prince.com>
2019-09-28 23:25:28 +0900

    proptypes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import PropTypes from 'prop-types';
 // function App() {
 //   return (
 //     <div><h1>Hello World</h1></div>
@@ -15,15 +16,30 @@ import React,{Component} from 'react';
 //   }
 // }
 const App = () =>{
+  const profiles = [
+    {name:"Taro", age:10},
+    {name: "Hanako",age:5},
+    {name:1}
+  ]
   return (<div>
-    <Cat />
-    <Cat />
-    <Cat />
+    {
+      profiles.map((profile,index) => {
+        return <User name={profile.name} age={profile.age} key={index}></User>
+      })
+    }
   </div>
     )
 }
+const User = (props) => {
+  return <div>Hi! {props.name} {props.age}</div>
+}
 const Cat = () =>{
   return <div>Meow!</div>
+}
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 export default App;
 　
