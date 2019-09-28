609ada367afead4d8947916415604588dfdd1046
akamatsu <akamatsu@star-prince.com>
2019-09-28 18:51:38 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
-function App() {
-  return (
-    <div><h1>Hello World</h1></div>
-  );
+import React,{Component} from 'react';
+// function App() {
+//   return (
+//     <div><h1>Hello World</h1></div>
+//   );
+// }
+class App extends Component{
+  render() {
+    return <h1>Hello, world!</h1>
+  }
 }
-
 export default App;
 　
